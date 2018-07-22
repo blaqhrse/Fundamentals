a0ced9e10c675085e5e8403b6147376ab0535ef3
Zack Macharia <macharia@zacks-mbp.attlocal.net>
2018-07-21 23:21:23 -0500

    Initial commit

---
+++ the_new_york_game.py
@@ -0,0 +1,11 @@
+"""things we want to do in new w york.code by milan macharia age 7"""
+
+import random
+
+activities = ["american girl doll store", "watch lion king", "wax museum", "rockefeller",
+              "brooklyn bridge ", "time square ", "1 tower", "statue of liberty", "fashion district"
+              "central park"]
+
+computer_guess = random.choice(activities)
+print("Computer chooses: ", computer_guess)
+
